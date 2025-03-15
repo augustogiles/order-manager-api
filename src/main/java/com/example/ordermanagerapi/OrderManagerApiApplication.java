83123db19ad7970624701d23d902541950e643ad
augustogiles <augustogiles@gmail.com>
2025-03-15 21:55:44 +0000

    Initializing project

---
+++ src/main/java/com/example/ordermanagerapi/OrderManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordermanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ordermanagerapi/OrderManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordermanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
